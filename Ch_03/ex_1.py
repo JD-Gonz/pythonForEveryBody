70102738e1eba03e939981d66746e8fed5eccf89
JD Gonzalez <dgonz001@uw.edu>
2023-03-02 13:53:16 -0800

    Add chapter 3 solutions

---
+++ Ch_03/ex_1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+pay = pay = hours * rate
+if hours > 40 :
+    overtime = (hours - 40) * rate * 1.5
+    pay = 40 * rate + overtime
+print("Pay:", pay)
---
+++ Ch_03/ex_2.py
@@ -0,0 +1,15 @@
+# Exercise 2: Rewrite your pay program using try and except 
+# so that your program handles non-numeric input gracefully
+# by printing a message and exiting the program.
+try :
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))    
+except :
+    print("Error, please enter numeric input")
+    quit()
+    
+pay = pay = hours * rate
+if hours > 40 :
+    overtime = (hours - 40) * rate * 1.5
+    pay = 40 * rate + overtime
+print("Pay:", pay)
