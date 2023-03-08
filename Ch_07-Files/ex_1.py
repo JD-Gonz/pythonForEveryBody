b4764af326acfa211ee5cac8d516bc24b076038c
JD Gonzalez <dgonz001@uw.edu>
2023-03-08 13:34:31 -0800

    use forwards slash for file look up

--- Ch_07-Files/ex_1.py
+++ Ch_07-Files/ex_1.py
@@ -1,4 +1,4 @@
 # Exercise 1: Write a program to read through a file and print the contents of the file (line by line) all in upper case. 
-fh = open('..\mbox-short.txt')
+fh = open('../mbox-short.txt')
 for line in fh :
     print(line.rstrip().upper())
